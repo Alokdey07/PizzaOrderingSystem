06376235fca9ddfa91fca9177cc9d8ab3a315606
Bhawna gupta <81208710+bhawna8teen@users.noreply.github.com>
2024-04-16 12:23:45 +0530

    CustomerEntity Added
    CustomerEntity Added

--- PizzaOrderingSystem-main/src/main/java/com/vw/pizza/entity/Customer.java
+++ PizzaOrderingSystem-main/src/main/java/com/vw/pizza/entity/Customer.java
@@ -1,5 +1,70 @@
 package com.vw.pizza.entity;
 
-public class Customer {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	public Customer() {
+		super();
+	}
+	public Customer(int id, String fname, String lname, String address, String email, Long mobile) {
+		this.id = id;
+		this.fname = fname;
+		this.lname = lname;
+		this.address = address;
+		this.email = email;
+		this.mobile = mobile;
+	}
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", fname=" + fname + ", lname=" + lname + ", address=" + address + ", email="
+				+ email + ", mobile=" + mobile + "]";
+	}
+	private String fname;
+	private String lname;
+	private String address;
+	private String email;
+	private Long mobile;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Long getMobile() {
+		return mobile;
+	}
+	public void setMobile(Long mobile) {
+		this.mobile = mobile;
+	}
 }
